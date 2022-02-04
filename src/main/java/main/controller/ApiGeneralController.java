a3a631e813ba030b731b8c38eb24a95f5cc4538f
@maksim_dimitrov <maxdemicus@inbox.ru>
2022-02-05 01:00:41 +0800

    Get_Init

--- src/main/java/main/controller/ApiGeneralController.java
+++ src/main/java/main/controller/ApiGeneralController.java
@@ -1,7 +1,22 @@
 package main.controller;
 
+import main.response.InitResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiGeneralController {
+
+    private final InitResponse initResponse;
+
+    public ApiGeneralController(InitResponse initResponse) {
+        this.initResponse = initResponse;
+    }
+
+    @GetMapping("/init")
+    private InitResponse init(){
+        return initResponse;
+    }
 }
